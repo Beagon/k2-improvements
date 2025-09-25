e1bdea3e13b7798ec3034689a98ff4f2134bd7ce
Robin Rijkeboer <robin@richblox.nl>
2025-09-25 16:12:47 +0200

    feature(SWAP): Add crontab to no kill master-server and klippy

--- features/add-swap/install.sh
+++ features/add-swap/install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
 # Create swapfile
 echo "Creating 256MB Swapfile, this can take a while!"
@@ -10,5 +11,6 @@ uci set fstab.@swap[-1].device='/mnt/UDISK/swapfile'
 uci set fstab.@swap[-1].enabled='1'
 uci commit fstab
 /etc/init.d/fstab restart
+crontab ${SCRIPT_DIR}/crontab
 echo "Done, enjoy your extra memory."
 
